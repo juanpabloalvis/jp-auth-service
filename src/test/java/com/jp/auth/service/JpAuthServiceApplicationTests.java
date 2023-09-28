516e9ce38efd6772e8fe2ed6bd8d8a964e7cd0bc
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-28 09:58:06 -0500

    Initial Commit

---
+++ src/test/java/com/jp/auth/service/JpAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
