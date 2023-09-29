e6b6372196d56d787391b66dc866fe7ca825f92f
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-28 20:17:45 -0500

    Create autentication service with createUser, login, and validate token

--- src/main/java/com/jp/auth/config/JwtProvider.java
+++ src/main/java/com/jp/auth/config/JwtProvider.java
@@ -21,53 +21,9 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @Component
 @Log4j2
 public class JwtProvider {
+
     @Value("${jwt.secret}")
     private String secret;
-
-//    public String createTokenBk(UserEntity user) {
-//        Map<String, Object> claims = Jwts.claims()
-//                .setSubject(user.getUsername());
-//        claims.put("id", user.getId());
-//        // TODO remove hardcode
-////        claims.put("twitter_account", "@juanpabloalvis");
-//        Instant now = Instant.now();
-////        Date exp = new Date(now.getTime() + 3600 * 1000);
-//        Instant exp = now.plusMillis(3600 * 1000);
-//
-//
-//        // Create a test key suitable for the desired HMAC-SHA algorithm:
-//        MacAlgorithm alg = Jwts.SIG.HS512; //or HS384 or HS256
-//        SecretKey key = alg.key().build();
-//
-//        String message = "Hello World!";
-//        byte[] content = message.getBytes(StandardCharsets.UTF_8);
-//
-////// Create the compact JWS:
-////        String jws = Jwts.builder().content(content, "text/plain").signWith(key, alg).compact();
-////
-////// Parse the compact JWS:
-////        content = Jwts.parser().verifyWith(key).build().parseContentJws(jws).getPayload();
-////
-////
-////
-////
-//////        String encodedSecret = Encoders.BASE64.encode(secret.getBytes());
-//////        byte[] keyBytes = Decoders.BASE64.decode(encodedSecret);
-//////        Key key = Keys.hmacShaKeyFor(keyBytes);
-////        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
-////        Key key = Keys.hmacShaKeyFor(secret.getBytes());
-////        jwtBuilder.signWith(key); //or signWith(Key, SignatureAlgorithm)
-//
-//        return Jwts.builder()
-//                // TODO remove hardcode
-//                .setHeaderParam("company_name", "devs4j")
-//                .setClaims(claims)
-//                .setIssuedAt(Date.from(now))
-//                .setExpiration(Date.from(exp))
-//                .signWith(SignatureAlgorithm.HS256, secret)
-//                .signWith(key)
-//                .compact();
-//    }
 
     public String createToken(UserEntity user) {
         Map<String, Object> claims = Jwts.claims()
@@ -76,28 +32,19 @@ public class JwtProvider {
         Date now = new Date();
 
         Date expiration = new Date(now.getTime() + (1000 * 60 * 60));  // 1 hora
-
-
         try {
             SecretKeySpec secretKeySpec = getSecretKeySpec();
-
-            //        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
-//            Key key = Keys.hmacShaKeyFor(secret.getBytes());
-
             return Jwts.builder()
                     .setSubject(user.getUsername())
                     .setIssuedAt(now)
                     .setClaims(claims)
                     .setExpiration(expiration)
-//                .signWith(SignatureAlgorithm.HS256, secret)// deprecado
                     .signWith(secretKeySpec)
                     .compact();
 
         } catch (NoSuchAlgorithmException | InvalidKeyException exception) {
             throw new ResponseStatusException(UNAUTHORIZED);
         }
-
-
     }
 
     private SecretKeySpec getSecretKeySpec() throws NoSuchAlgorithmException, InvalidKeyException {
